734a3cc32fe762cbd02f245a0598a1e62539fb16
far-id <fcrizkywijaya@gmail.com>
2023-07-12 17:11:29 +0700

    user account seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Employee;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -14,19 +15,37 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $employee = Employee::create([
-            'name' => 'Farid Rizky Wijaya',
-            'email' => 'admin@mail.test',
-            'address' => fake()->address(),
-            'branch_id' => rand(1, 3)
-        ]);
-        \App\Models\User::insert([
-            'name' => $employee->name,
-            'email' => $employee->email,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-            'employee_id' => $employee->id,
-        ]);
+        collect([
+            [
+                'name' => 'Farid Rizky Wijaya',
+                'email' => 'farid@mail.test',
+                'address' => fake()->address(),
+                'branch_id' => rand(1, 3)
+            ],
+            [
+
+                'name' => 'Adam Julizar',
+                'email' => 'adam@mail.test',
+                'address' => fake()->address(),
+                'branch_id' => rand(1, 3)
+            ],
+            [
+
+                'name' => 'Falentino Rusdianto',
+                'email' => 'falen@mail.test',
+                'address' => fake()->address(),
+                'branch_id' => rand(1, 3)
+            ]
+        ])->each(function ($employee) {
+            $employee = Employee::create($employee);
+            User::create([
+                'name' => $employee->name,
+                'email' => $employee->email,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'employee_id' => $employee->id,
+            ]);
+        });
     }
 }
